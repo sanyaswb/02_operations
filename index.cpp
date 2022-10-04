81645662f78f4c55d29f7a6f3a42eb83ae9a3f07
Александр Меркулов <merkoolow@Aleksandrs-MacBook-Pro.local>
2022-10-05 01:21:36 +0300

    complete task_02

--- index.cpp
+++ index.cpp
@@ -2,5 +2,12 @@
 #include <string>
 
 int operations(int num1, int num2, int num3) {
-	return 0;
+
+	int result1 = num1 + num3;
+	int result2 = num2 - num3;
+	int result3 = result1 * 6;
+	int result4 = result2 * 2;
+	int result5 = result3 + result4;
+
+	return result5;
 }
